b3b0b396fbb2b2e0b9e87f88888f1edcfa6d91c8
tyagiman <88586813+tyagiman@users.noreply.github.com>
2022-10-28 06:55:29 +0200

    Add files via upload

---
+++ Numpy1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 17:04:31 2022
+
+@author: priyankagharpure
+"""
+
+import numpy as np
+
+height=[5.7,6,5.8,6.2,5.5]
+
+print(height)
+
+type(height)
+
+print(height*30)
+
+np_height=np.array([5.7,6,5.8,6.2,5.5])
+
+type(np_height)
+
+print(np_height*30)
+
+
+np_height[:2]
